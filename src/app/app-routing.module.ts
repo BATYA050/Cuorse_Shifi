ae9c005e7a4d063cfc57f7127a92d45fb4dd6f65
BATYA050 <H.BATYA.D@GMAIL.COM>
2023-06-21 16:00:04 +0300

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Schedule } from './Schedule_s/Schedule';
-
+// import { AuthGuard } from 'src/app/auth.guard';
 
 
 const routes: Routes = [
